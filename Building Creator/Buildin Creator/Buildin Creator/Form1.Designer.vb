88c1ef98ac73ad0d262eb8aa08f7213bd6dff365
Todd Aden <45721115+FluentZap@users.noreply.github.com>
2014-06-18 00:00:46 -0700

    Improved Building Creator Program

--- Building Creator/Buildin Creator/Buildin Creator/Form1.Designer.vb
+++ Building Creator/Buildin Creator/Buildin Creator/Form1.Designer.vb
@@ -27,7 +27,6 @@ Partial Class Form1
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
         Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
         Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
-        Me.LoadBitmapToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
         Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
         Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
         Me.TypeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
@@ -35,14 +34,13 @@ Partial Class Form1
         Me.RectHToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
         Me.SmallToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
         Me.LargeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
-        Me.Label3 = New System.Windows.Forms.Label
-        Me.TextBox3 = New System.Windows.Forms.TextBox
-        Me.Label2 = New System.Windows.Forms.Label
-        Me.Label4 = New System.Windows.Forms.Label
-        Me.Label1 = New System.Windows.Forms.Label
-        Me.Label5 = New System.Windows.Forms.Label
-        Me.Label6 = New System.Windows.Forms.Label
         Me.Label7 = New System.Windows.Forms.Label
+        Me.WalkableBox = New System.Windows.Forms.ListBox
+        Me.SpriteBox = New System.Windows.Forms.ListBox
+        Me.TileBox = New System.Windows.Forms.ListBox
+        Me.ScrollV = New System.Windows.Forms.VScrollBar
+        Me.ScrollH = New System.Windows.Forms.HScrollBar
+        Me.SetAll = New System.Windows.Forms.Button
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -55,13 +53,13 @@ Partial Class Form1
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.TypeToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(1194, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1384, 24)
         Me.MenuStrip1.TabIndex = 4
         Me.MenuStrip1.Text = "MenuStrip1"
         '
         'FileToolStripMenuItem
         '
-        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.LoadBitmapToolStripMenuItem, Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem})
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem})
         Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
         Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
         Me.FileToolStripMenuItem.Text = "File"
@@ -71,12 +69,6 @@ Partial Class Form1
         Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
         Me.NewToolStripMenuItem.Size = New System.Drawing.Size(141, 22)
         Me.NewToolStripMenuItem.Text = "New"
-        '
-        'LoadBitmapToolStripMenuItem
-        '
-        Me.LoadBitmapToolStripMenuItem.Name = "LoadBitmapToolStripMenuItem"
-        Me.LoadBitmapToolStripMenuItem.Size = New System.Drawing.Size(141, 22)
-        Me.LoadBitmapToolStripMenuItem.Text = "Load Bitmap"
         '
         'OpenToolStripMenuItem
         '
@@ -121,77 +113,6 @@ Partial Class Form1
         Me.LargeToolStripMenuItem.Size = New System.Drawing.Size(106, 22)
         Me.LargeToolStripMenuItem.Text = "Large"
         '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(139, 7)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(31, 13)
-        Me.Label3.TabIndex = 13
-        Me.Label3.Text = "Type"
-        '
-        'TextBox3
-        '
-        Me.TextBox3.Location = New System.Drawing.Point(179, 4)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox3.TabIndex = 12
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.BackColor = System.Drawing.Color.LawnGreen
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(1030, 116)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(74, 20)
-        Me.Label2.TabIndex = 14
-        Me.Label2.Text = "Walkable"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.BackColor = System.Drawing.Color.Gray
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(1030, 136)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(91, 20)
-        Me.Label4.TabIndex = 15
-        Me.Label4.Text = "Impassable"
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.BackColor = System.Drawing.Color.Gray
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(1030, 254)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(60, 20)
-        Me.Label1.TabIndex = 17
-        Me.Label1.Text = "Sprite2"
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.BackColor = System.Drawing.Color.LawnGreen
-        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(1030, 234)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(51, 20)
-        Me.Label5.TabIndex = 16
-        Me.Label5.Text = "Sprite"
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.BackColor = System.Drawing.Color.Gray
-        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(1030, 156)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(47, 20)
-        Me.Label6.TabIndex = 18
-        Me.Label6.Text = "None"
-        '
         'Label7
         '
         Me.Label7.AutoSize = True
@@ -201,24 +122,83 @@ Partial Class Form1
         Me.Label7.TabIndex = 19
         Me.Label7.Text = "Label7"
         '
+        'WalkableBox
+        '
+        Me.WalkableBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.WalkableBox.FormattingEnabled = True
+        Me.WalkableBox.ItemHeight = 24
+        Me.WalkableBox.Items.AddRange(New Object() {"Walkable", "Impassable", "None"})
+        Me.WalkableBox.Location = New System.Drawing.Point(1227, 27)
+        Me.WalkableBox.Name = "WalkableBox"
+        Me.WalkableBox.Size = New System.Drawing.Size(157, 76)
+        Me.WalkableBox.TabIndex = 21
+        '
+        'SpriteBox
+        '
+        Me.SpriteBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SpriteBox.FormattingEnabled = True
+        Me.SpriteBox.ItemHeight = 24
+        Me.SpriteBox.Items.AddRange(New Object() {"Sprite", "Sprite2", "None"})
+        Me.SpriteBox.Location = New System.Drawing.Point(1227, 109)
+        Me.SpriteBox.Name = "SpriteBox"
+        Me.SpriteBox.Size = New System.Drawing.Size(157, 76)
+        Me.SpriteBox.TabIndex = 22
+        '
+        'TileBox
+        '
+        Me.TileBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TileBox.FormattingEnabled = True
+        Me.TileBox.ItemHeight = 16
+        Me.TileBox.Location = New System.Drawing.Point(1227, 191)
+        Me.TileBox.Name = "TileBox"
+        Me.TileBox.Size = New System.Drawing.Size(157, 356)
+        Me.TileBox.TabIndex = 23
+        '
+        'ScrollV
+        '
+        Me.ScrollV.LargeChange = 2
+        Me.ScrollV.Location = New System.Drawing.Point(0, 24)
+        Me.ScrollV.Maximum = 102
+        Me.ScrollV.Name = "ScrollV"
+        Me.ScrollV.Size = New System.Drawing.Size(20, 951)
+        Me.ScrollV.TabIndex = 24
+        '
+        'ScrollH
+        '
+        Me.ScrollH.LargeChange = 2
+        Me.ScrollH.Location = New System.Drawing.Point(19, 975)
+        Me.ScrollH.Maximum = 92
+        Me.ScrollH.Name = "ScrollH"
+        Me.ScrollH.Size = New System.Drawing.Size(1025, 20)
+        Me.ScrollH.TabIndex = 25
+        '
+        'SetAll
+        '
+        Me.SetAll.Location = New System.Drawing.Point(1227, 553)
+        Me.SetAll.Name = "SetAll"
+        Me.SetAll.Size = New System.Drawing.Size(157, 37)
+        Me.SetAll.TabIndex = 26
+        Me.SetAll.Text = "Set All"
+        Me.SetAll.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1194, 1064)
+        Me.ClientSize = New System.Drawing.Size(1384, 994)
+        Me.Controls.Add(Me.SetAll)
+        Me.Controls.Add(Me.ScrollH)
+        Me.Controls.Add(Me.ScrollV)
+        Me.Controls.Add(Me.TileBox)
+        Me.Controls.Add(Me.SpriteBox)
+        Me.Controls.Add(Me.WalkableBox)
         Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.MenuStrip1)
         Me.DoubleBuffered = True
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
@@ -238,14 +218,12 @@ Partial Class Form1
     Friend WithEvents RectHToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents SmallToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents LargeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
-    Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
-    Friend WithEvents LoadBitmapToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Label4 As System.Windows.Forms.Label
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label5 As System.Windows.Forms.Label
-    Friend WithEvents Label6 As System.Windows.Forms.Label
     Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents WalkableBox As System.Windows.Forms.ListBox
+    Friend WithEvents SpriteBox As System.Windows.Forms.ListBox
+    Friend WithEvents TileBox As System.Windows.Forms.ListBox
+    Friend WithEvents ScrollV As System.Windows.Forms.VScrollBar
+    Friend WithEvents ScrollH As System.Windows.Forms.HScrollBar
+    Friend WithEvents SetAll As System.Windows.Forms.Button
 
 End Class
